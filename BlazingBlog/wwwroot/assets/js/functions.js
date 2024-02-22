ba56c22083da769a672088f99f4de33b855b5bba
Wael Muawad <wael.muawad@gmail.com>
2024-02-22 11:42:08 +0300

    Add project files.

---
+++ BlazingBlog/wwwroot/assets/js/functions.js
@@ -0,0 +1,28 @@
+//jQuery( document ).ready(function() {
+
+//    $(window).scroll(function(){
+//    $('.topnav').toggleClass('scrollednav py-0', $(this).scrollTop() > 50);
+//    });
+    
+//});
+
+
+document.addEventListener("DOMContentLoaded", _ => {
+    const topNav = document.getElementsByClassName("topnav")[0];
+    if (topNav) {
+        window.onscroll = () => {
+            if (window.scrollY > 50) {
+                topNav.classList.add("scrollednav", "py-0")
+            }
+            else {
+                topNav.classList.remove("scrollednav", "py-0")
+            }
+        }
+    }
+})
+
+function ToggleMenu(e){
+    e.target.classList.toggle("collapsed");
+    const navbarWrapper = document.getElementById("top-navbar-menu-wrapper");
+    navbarWrapper.classList.toggle("show");
+}
